f8b163b25d1408f69db9426618a26b43c1b5dadf
Brett Fowle <brettfowle@gmail.com>
2018-12-19 17:35:07 -0500

    refactor large functions into smaller, logical ones

--- pkg/controller/januscontroller.go
+++ pkg/controller/januscontroller.go
@@ -307,56 +307,7 @@ func (jgc *JanusGuardController) addPod(obj interface{}) {
 	// running on the same node as the daemon, if they match our nodeSelector
 	// then immediately enqueue the JanusGuard for additions.
 	if label, _ := pod.Labels["daemon"]; label == "janusd" {
-		var hostURL string
-		// Run this function with a retry, to make sure we get a connection to
-		// the daemon pod. If we exhaust all attempts, process error
-		// accordingly.
-		if retryErr := retry.RetryOnConflict(jgc.backoff, func() (err error) {
-			po, err := jgc.podLister.Pods(janusNamespace).Get(pod.Name)
-			if err != nil {
-				err = errorsutil.NewConflict(schema.GroupResource{Resource: "pods"},
-					po.Name, errors.New("could not find pod"))
-				return err
-			}
-			hostURL, err = jgc.getHostURL(po)
-			if err != nil {
-				err = errorsutil.NewConflict(schema.GroupResource{Resource: "pods"},
-					po.Name, errors.New("pod host is not available"))
-				return err
-			}
-			// Initialize connection to gRPC server on daemon.
-			opts, err := BuildAndStoreDialOptions(TLS, TLSSkipVerify, TLSCACert, TLSClientCert, TLSClientKey, TLSServerName)
-			if err != nil {
-				return err
-			}
-			conn, err := NewJanusdConnection(hostURL, opts)
-			if err != nil {
-				return err
-			}
-			jgc.janusdConnections.Store(hostURL, conn)
-			return err
-		}); retryErr != nil {
-			return
-		}
-
-		allPods, err := jgc.podLister.List(labels.Everything())
-		if err != nil {
-			return
-		}
-		for _, po := range allPods {
-			if po.Spec.NodeName != pod.Spec.NodeName {
-				continue
-			}
-			jgs := jgc.getPodJanusGuards(po)
-			if len(jgs) == 0 {
-				continue
-			}
-
-			glog.V(4).Infof("Unannotated pod %s found: %#v.", po.Name, po)
-			for _, jg := range jgs {
-				jgc.enqueueJanusGuard(jg)
-			}
-		}
+		jgc.addDaemonPod(pod)
 		return
 	}
 
@@ -370,6 +321,63 @@ func (jgc *JanusGuardController) addPod(obj interface{}) {
 	glog.V(4).Infof("Unannotated pod %s found: %#v.", pod.Name, pod)
 	for _, jg := range jgs {
 		jgc.enqueueJanusGuard(jg)
+	}
+}
+
+// addDaemonPod initializes the connection to the gRPC server run on the
+// daemon. Then a check is done on any pods running on the same node as the
+// daemon, if they match our nodeSelector then immediately enqueue the
+// JanusGuard for additions.
+func (jgc *JanusGuardController) addDaemonPod(pod *corev1.Pod) {
+	var hostURL string
+	// Run this function with a retry, to make sure we get a connection to
+	// the daemon pod. If we exhaust all attempts, process error
+	// accordingly.
+	if retryErr := retry.RetryOnConflict(jgc.backoff, func() (err error) {
+		po, err := jgc.podLister.Pods(janusNamespace).Get(pod.Name)
+		if err != nil {
+			err = errorsutil.NewConflict(schema.GroupResource{Resource: "pods"},
+				po.Name, errors.New("could not find pod"))
+			return err
+		}
+		hostURL, err = jgc.getHostURL(po)
+		if err != nil {
+			err = errorsutil.NewConflict(schema.GroupResource{Resource: "pods"},
+				po.Name, errors.New("pod host is not available"))
+			return err
+		}
+		// Initialize connection to gRPC server on daemon.
+		opts, err := BuildAndStoreDialOptions(TLS, TLSSkipVerify, TLSCACert, TLSClientCert, TLSClientKey, TLSServerName)
+		if err != nil {
+			return err
+		}
+		conn, err := NewJanusdConnection(hostURL, opts)
+		if err != nil {
+			return err
+		}
+		jgc.janusdConnections.Store(hostURL, conn)
+		return err
+	}); retryErr != nil {
+		return
+	}
+
+	allPods, err := jgc.podLister.List(labels.Everything())
+	if err != nil {
+		return
+	}
+	for _, po := range allPods {
+		if po.Spec.NodeName != pod.Spec.NodeName {
+			continue
+		}
+		jgs := jgc.getPodJanusGuards(po)
+		if len(jgs) == 0 {
+			continue
+		}
+
+		glog.V(4).Infof("Unannotated pod %s found: %#v.", po.Name, po)
+		for _, jg := range jgs {
+			jgc.enqueueJanusGuard(jg)
+		}
 	}
 }
 
@@ -592,11 +600,6 @@ func (jgc *JanusGuardController) syncJanusGuard(key string) error {
 
 	jgNeedsSync := jgc.expectations.SatisfiedExpectations(key)
 
-	// Get the diff between all pods and pods that match the JanusGuard
-	// selector.
-	var rmPods []*corev1.Pod
-	var addPods []*corev1.Pod
-
 	selector, err := metav1.LabelSelectorAsSelector(jg.Spec.Selector)
 	if err != nil {
 		runtime.HandleError(fmt.Errorf("Error converting pod selector to selector: %v", err))
@@ -607,58 +610,11 @@ func (jgc *JanusGuardController) syncJanusGuard(key string) error {
 		return err
 	}
 
-	// @TODO: Only get pods with annotation: JanusGuardAnnotationKey.
-	allPods, err := jgc.podLister.Pods(jg.Namespace).List(labels.Everything())
+	// Get the diff between all pods and pods that match the JanusGuard
+	// selector.
+	addPods, rmPods, err := jgc.getPodDiff(selectedPods, jg)
 	if err != nil {
 		return err
-	}
-
-	// Get current guard state from JanusD daemon.
-	guardStates, err := jgc.getGuardStates()
-	if err != nil {
-		return err
-	}
-
-	for _, pod := range selectedPods {
-		if pod.DeletionTimestamp != nil ||
-			!podutil.IsPodReady(pod) {
-			continue
-		}
-		if wsFound := jgc.isPodInGuardState(pod, guardStates); !wsFound {
-			var found bool
-			updatePodQueue.Range(func(k, v interface{}) bool {
-				// Check if pod is already in updatePodQueue.
-				if pod.Name == k {
-					found = true
-					return false
-				}
-				return true
-			})
-			if !found {
-				addPods = append(addPods, pod)
-				continue
-			}
-		}
-	}
-
-	for _, pod := range allPods {
-		if wsFound := jgc.isPodInGuardState(pod, guardStates); !wsFound {
-			continue
-		}
-
-		var selFound bool
-		for _, po := range selectedPods {
-			if pod.Name == po.Name {
-				selFound = true
-				break
-			}
-		}
-		if pod.DeletionTimestamp != nil || !selFound {
-			if value, found := pod.Annotations[JanusGuardAnnotationKey]; found && value == jg.Name {
-				rmPods = append(rmPods, pod)
-				continue
-			}
-		}
 	}
 
 	var manageSubjectsErr error
@@ -814,13 +770,86 @@ func (jgc *JanusGuardController) getPodJanusGuards(pod *corev1.Pod) []*janusv1al
 	return jgs
 }
 
-// getPodKeys returns a list of pod key strings from array of pod objects.
-func getPodKeys(pods []*corev1.Pod) []string {
-	podKeys := make([]string, 0, len(pods))
-	for _, pod := range pods {
-		podKeys = append(podKeys, controller.PodKey(pod))
+// getPodDiff gets the difference between all pods and pods that match the
+// JanusGuard selector in the form of two slices of pod types.
+func (jgc *JanusGuardController) getPodDiff(selectedPods []*corev1.Pod, jg *janusv1alpha1.JanusGuard) ([]*corev1.Pod, []*corev1.Pod, error) {
+	var rmPods []*corev1.Pod
+	var addPods []*corev1.Pod
+
+	// Get current guard state from JanusD daemon.
+	guardStates, err := jgc.getGuardStates()
+	if err != nil {
+		return nil, nil, err
 	}
-	return podKeys
+
+	jgc.resolveAddPods(&addPods, selectedPods, guardStates)
+	if err := jgc.resolveRmPods(&rmPods, selectedPods, guardStates, jg); err != nil {
+		return nil, nil, err
+	}
+
+	return addPods, rmPods, nil
+}
+
+// resolveAddPods correlates between pods that should be watched and those that
+// the latest state check from the daemon reported were currently being
+// watched.
+func (jgc *JanusGuardController) resolveAddPods(addPods *[]*corev1.Pod, selectedPods []*corev1.Pod, guardStates [][]*pb.JanusdHandle) {
+	for _, pod := range selectedPods {
+		if pod.DeletionTimestamp != nil ||
+			!podutil.IsPodReady(pod) {
+			continue
+		}
+		if wsFound := jgc.isPodInGuardState(pod, guardStates); !wsFound {
+			var found bool
+			updatePodQueue.Range(func(k, v interface{}) bool {
+				// Check if pod is already in updatePodQueue.
+				if pod.Name == k {
+					found = true
+					return false
+				}
+				return true
+			})
+			if !found {
+				*addPods = append(*addPods, pod)
+				continue
+			}
+		}
+	}
+}
+
+// resolveRmPods correlates between pods that are currently being watched and
+// those that the latest state check from the daemon reported were currently
+// being watched.
+func (jgc *JanusGuardController) resolveRmPods(rmPods *[]*corev1.Pod, selectedPods []*corev1.Pod,
+	guardStates [][]*pb.JanusdHandle, jg *janusv1alpha1.JanusGuard) error {
+
+	// @TODO: Only get pods with annotation: JanusGuardAnnotationKey.
+	allPods, err := jgc.podLister.Pods(jg.Namespace).List(labels.Everything())
+	if err != nil {
+		return err
+	}
+
+	for _, pod := range allPods {
+		if wsFound := jgc.isPodInGuardState(pod, guardStates); !wsFound {
+			continue
+		}
+
+		var selFound bool
+		for _, po := range selectedPods {
+			if pod.Name == po.Name {
+				selFound = true
+				break
+			}
+		}
+		if pod.DeletionTimestamp != nil || !selFound {
+			if value, found := pod.Annotations[JanusGuardAnnotationKey]; found && value == jg.Name {
+				*rmPods = append(*rmPods, pod)
+				continue
+			}
+		}
+	}
+
+	return nil
 }
 
 // updatePodOnceValid first retries getting the pod hostURL to connect to the
@@ -828,12 +857,43 @@ func getPodKeys(pods []*corev1.Pod) []string {
 // the gRPC server CreateGuard function; on success it updates the appropriate
 // JanusGuard annotations so we can mark it now "guarded".
 func (jgc *JanusGuardController) updatePodOnceValid(podName string, jg *janusv1alpha1.JanusGuard) {
+	// Attempt to locate a daemon pod connection.
+	cids, hostURL, nodeName, retryErr := jgc.locateDaemonConnection(podName, jg)
+	if retryErr != nil {
+		return
+	}
+	// Attempt to add an JanusD guard with the located daemon pod connection,
+	// and get a successful response from the daemon.
+	if retryErr := jgc.attemptToAddJanusdGuard(cids, hostURL, nodeName, podName, jg); retryErr != nil {
+		updatePodWithRetries(jgc.kubeclientset.CoreV1().Pods(jg.Namespace), jgc.podLister,
+			jg.Namespace, podName, func(po *corev1.Pod) error {
+				pod, err := jgc.podLister.Pods(jg.Namespace).Get(podName)
+				if err != nil {
+					return err
+				}
+				po.Annotations = pod.Annotations
+				return nil
+			})
+	}
+
+	go jgc.removePodFromUpdateQueue(podName)
+
+	jgKey, err := controller.KeyFunc(jg)
+	if err != nil {
+		return
+	}
+	jgc.expectations.CreationObserved(jgKey)
+	jgc.recorder.Eventf(jg, corev1.EventTypeNormal, SuccessAdded, MessageResourceAdded, nodeName)
+}
+
+// locateDaemonConnection is run with a retry, to make sure we get a
+// connection to the daemon pod. If we exhaust all attempts, process error
+// accordingly.
+func (jgc *JanusGuardController) locateDaemonConnection(podName string, jg *janusv1alpha1.JanusGuard) ([]string, string, string, error) {
 	var cids []string
 	var nodeName, hostURL string
 
-	// Run this function with a retry, to make sure we get a connection to the
-	// daemon pod. If we exhaust all attempts, process error accordingly.
-	if retryErr := retry.RetryOnConflict(jgc.backoff, func() (err error) {
+	retryErr := retry.RetryOnConflict(jgc.backoff, func() (err error) {
 		// Be sure to clear all slice elements first in case of a retry.
 		cids = cids[:0]
 
@@ -869,14 +929,18 @@ func (jgc *JanusGuardController) updatePodOnceValid(podName string, jg *janusv1a
 				pod.Name, errors.New("pod host is not available"))
 		}
 		return err
-	}); retryErr != nil {
-		return
-	}
+	})
 
-	// Run this function with a retry, to make sure we get a successful
-	// response from the daemon. If we exhaust all attempts, process error
-	// accordingly.
-	if retryErr := retry.RetryOnConflict(jgc.backoff, func() (err error) {
+	return cids, hostURL, nodeName, retryErr
+}
+
+// attemptToAddJanusdGuard is run with a retry, to make sure we get a
+// successful response from the daemon. If we exhaust all attempts, process
+// error accordingly.
+func (jgc *JanusGuardController) attemptToAddJanusdGuard(cids []string, hostURL string,
+	nodeName string, podName string, jg *janusv1alpha1.JanusGuard) error {
+
+	retryErr := retry.RetryOnConflict(jgc.backoff, func() (err error) {
 		pod, err := jgc.podLister.Pods(jg.Namespace).Get(podName)
 		if err != nil {
 			return err
@@ -901,26 +965,9 @@ func (jgc *JanusGuardController) updatePodOnceValid(podName string, jg *janusv1a
 			fc.handle = handle
 		}
 		return err
-	}); retryErr != nil {
-		updatePodWithRetries(jgc.kubeclientset.CoreV1().Pods(jg.Namespace), jgc.podLister,
-			jg.Namespace, podName, func(po *corev1.Pod) error {
-				pod, err := jgc.podLister.Pods(jg.Namespace).Get(podName)
-				if err != nil {
-					return err
-				}
-				po.Annotations = pod.Annotations
-				return nil
-			})
-	}
+	})
 
-	go jgc.removePodFromUpdateQueue(podName)
-
-	jgKey, err := controller.KeyFunc(jg)
-	if err != nil {
-		return
-	}
-	jgc.expectations.CreationObserved(jgKey)
-	jgc.recorder.Eventf(jg, corev1.EventTypeNormal, SuccessAdded, MessageResourceAdded, nodeName)
+	return retryErr
 }
 
 // getHostURL constructs a URL from the pod's hostIP and hard-coded JanusD
@@ -1086,4 +1133,13 @@ func (jgc *JanusGuardController) isPodInGuardState(pod *corev1.Pod, guardStates 
 // future.
 func (jgc *JanusGuardController) removePodFromUpdateQueue(podName string) {
 	updatePodQueue.Delete(podName)
+}
+
+// getPodKeys returns a list of pod key strings from array of pod objects.
+func getPodKeys(pods []*corev1.Pod) []string {
+	podKeys := make([]string, 0, len(pods))
+	for _, pod := range pods {
+		podKeys = append(podKeys, controller.PodKey(pod))
+	}
+	return podKeys
 }
